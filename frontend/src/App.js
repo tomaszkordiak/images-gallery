23f09b14936ffc8464f89d9328acacd0d3aef255
Tomasz Kordiak <tkordiak@gmail.com>
2021-07-11 17:42:08 +0200

    search component changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
 
+
+const handleSearchSubmit = (e) => {
+  e.preventDefault();
+  console.log(e);
+}
 
 const App = () => {
   return (
     <div>
       <Header title="Images Gallery" />
+      <Search handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,28 @@
+//import { Button } from "bootstrap";
+import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+const Search = ({ handleSubmit }) => {
+  return (
+      <Container className="mt-4">
+        <Row className="justify-content-center">
+          <Col xs={12} md={8} lg={6}>
+            <Form onSubmit={handleSubmit}>
+              <Form.Row>
+                <Col xs={9}>
+                  <Form.Control placeholder="Search for new image..." />
+                </Col>
+                <Col>
+                  <Button variant="primary" type="submit">
+                    Search
+                  </Button>
+                </Col>
+              </Form.Row>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+  );
+};
+
+export default Search;
