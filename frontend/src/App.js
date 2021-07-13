0dc85ed49da870e85a1e85a23105c494924f6f8b
Tomasz Kordiak <tkordiak@gmail.com>
2021-07-13 10:51:48 +0200

    format code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
-const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((result) => result.json())
       .then((data) => {
         console.log(data);
       })
-      .catch((error) => {console.log(error);})
-    setWord('');
-  }
-
+      .catch((error) => {
+        console.log(error);
+      });
+    setWord("");
+  };
 
   return (
     <div>
@@ -27,6 +30,6 @@ const App = () => {
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
